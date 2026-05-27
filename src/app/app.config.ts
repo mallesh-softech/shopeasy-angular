5ac7c9cad8e80757321fbf33b922fbb096c69633
mallesh-softech <mallesh@softechgroupus.com>
2026-05-27 16:48:52 +0530

    initial ecommerce app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ import { authInterceptor } from './core/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
     provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,100 +6,138 @@ export const routes: Routes = [
   {
     path: 'auth',
     loadComponent: () =>
-      import('./layouts/auth-layout/auth-layout.component').then((m) => m.AuthLayoutComponent),
+      import('./layouts/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
+      ),
     canActivate: [guestGuard],
     children: [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
+
       {
         path: 'login',
         loadComponent: () =>
-          import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+          import('./features/auth/login/login.component').then(
+            (m) => m.LoginComponent
+          ),
       },
+
       {
         path: 'signup',
         loadComponent: () =>
-          import('./features/auth/signup/signup.component').then((m) => m.SignupComponent),
+          import('./features/auth/signup/signup.component').then(
+            (m) => m.SignupComponent
+          ),
       },
     ],
   },
+
   {
     path: '',
-    canActivate: [authGuard],
+    // TEMPORARILY REMOVED authGuard FOR GITHUB PAGES
     loadComponent: () =>
-      import('./layouts/main-layout/main-layout.component').then((m) => m.MainLayoutComponent),
+      import('./layouts/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
+      ),
+
     children: [
       {
         path: '',
         loadComponent: () =>
-          import('./pages/customer/home/home.component').then((m) => m.HomeComponent),
+          import('./pages/customer/home/home.component').then(
+            (m) => m.HomeComponent
+          ),
       },
+
       {
         path: 'products',
         loadComponent: () =>
-          import('./pages/customer/products/product-list/product-list.component').then(
-            (m) => m.ProductListComponent,
-          ),
+          import(
+            './pages/customer/products/product-list/product-list.component'
+          ).then((m) => m.ProductListComponent),
       },
+
       {
         path: 'products/:id',
         loadComponent: () =>
-          import('./pages/customer/products/product-detail/product-detail.component').then(
-            (m) => m.ProductDetailComponent,
-          ),
+          import(
+            './pages/customer/products/product-detail/product-detail.component'
+          ).then((m) => m.ProductDetailComponent),
       },
+
       {
         path: 'cart',
         loadComponent: () =>
-          import('./pages/customer/cart/cart.component').then((m) => m.CartComponent),
+          import('./pages/customer/cart/cart.component').then(
+            (m) => m.CartComponent
+          ),
       },
+
       {
         path: 'wishlist',
         loadComponent: () =>
-          import('./pages/customer/wishlist/wishlist.component').then((m) => m.WishlistComponent),
+          import('./pages/customer/wishlist/wishlist.component').then(
+            (m) => m.WishlistComponent
+          ),
       },
+
       {
         path: 'checkout',
         canActivate: [authGuard],
         loadComponent: () =>
-          import('./pages/customer/checkout/checkout.component').then((m) => m.CheckoutComponent),
+          import('./pages/customer/checkout/checkout.component').then(
+            (m) => m.CheckoutComponent
+          ),
       },
+
       {
         path: 'payment',
         canActivate: [authGuard],
         loadComponent: () =>
-          import('./pages/customer/payment/payment.component').then((m) => m.PaymentComponent),
+          import('./pages/customer/payment/payment.component').then(
+            (m) => m.PaymentComponent
+          ),
       },
+
       {
         path: 'orders',
         canActivate: [authGuard],
         loadComponent: () =>
           import('./pages/customer/orders/order-list/order-list.component').then(
-            (m) => m.OrderListComponent,
+            (m) => m.OrderListComponent
           ),
       },
+
       {
         path: 'orders/confirmation/:id',
         canActivate: [authGuard],
         loadComponent: () =>
-          import('./pages/customer/orders/order-confirmation/order-confirmation.component').then(
-            (m) => m.OrderConfirmationComponent,
-          ),
+          import(
+            './pages/customer/orders/order-confirmation/order-confirmation.component'
+          ).then((m) => m.OrderConfirmationComponent),
       },
+
       {
         path: 'orders/track/:id',
         canActivate: [authGuard],
         loadComponent: () =>
-          import('./pages/customer/orders/order-tracking/order-tracking.component').then(
-            (m) => m.OrderTrackingComponent,
-          ),
+          import(
+            './pages/customer/orders/order-tracking/order-tracking.component'
+          ).then((m) => m.OrderTrackingComponent),
       },
+
       {
         path: 'profile',
         canActivate: [authGuard],
         loadComponent: () =>
-          import('./pages/customer/profile/profile.component').then((m) => m.ProfileComponent),
+          import('./pages/customer/profile/profile.component').then(
+            (m) => m.ProfileComponent
+          ),
       },
     ],
   },
-  { path: '**', redirectTo: '/auth/login' },
+
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
